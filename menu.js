eb22737b0955b3e1d390c4f1324fec56f1b4e9cf
gve123 <74577907+gve123@users.noreply.github.com>
2020-12-14 15:46:02 -0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,10 @@
+  document.write('<nav><ul>');
+  document.write('<li><a href="https://gve123.github.io/w1/">Inicio</a>');	
+  document.write('<li><a href="#">grupos</a><ul>');
+
+for (var k = 1 ; k <= 26; k++) {
+
+	document.write('<li><a href="https://gve123.github.io/w1/g'+k+'.html">grupo'+k+'</a></li>');
+}	
+	
+	document.write('</ul> </li></ul> </nav>');
