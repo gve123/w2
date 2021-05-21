dadc93147f7e6ab3599ea456082d4aa153c7802b
gve123 <74577907+gve123@users.noreply.github.com>
2021-05-21 12:32:07 -0500

    Update a2.js

--- a2.js
+++ a2.js
@@ -5,7 +5,7 @@
 let div1 = document.getElementById('contenedor1');
 
  
-for (var k = 1 ; k <= 80; k++) {
+for (var k = 1 ; k <= 81; k++) {
 
 	div1.innerHTML += 
     '<div class="numero" id="num'+k+'">'+k+'</div>'+
@@ -50,7 +50,7 @@ var hoy = new Date();
 var dia = hoy.getDate();
 var mes = hoy.getMonth() + 1;  
   
-for (var j = 1; j <= 80; j++) {
+for (var j = 1; j <= 81; j++) {
   
 	var checked = JSON.parse(localStorage.getItem("checkbox"+n+"#"+j));
     if (checked == true)
